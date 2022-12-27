a2ed5a6873c04545b2e007488a022e9445159952
AkaCyberRat <andrejchuk01@gmail.com>
2022-12-27 19:15:02 +0700

    Add core process model

---
+++ ProcessMonitor/App/Core/Process.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace App.Core
+{
+    internal class Process
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string MachineName { get; set; }
+        public string PriorityClass { get; set; }
+        public DateTime StartTime { get; set; }
+    }
+}
