fd15c1030768860c1a32b7e15a021b5e6540481c
AkaCyberRat <andrejchuk01@gmail.com>
2022-12-27 19:29:52 +0700

    Add IProcessService interface

---
+++ ProcessMonitor/App/Services/ProcessService/IProcessService.cs
@@ -0,0 +1,11 @@
+﻿using App.Core;
+
+namespace App.Services.ProcessService
+{
+    internal interface IProcessService
+    {
+        Process[] GetActiveProcesses();
+        Process GetProcessById(string id);
+        Process KillProcessById(string id);
+    }
+}
