fa5e0cd1c758c74459938842ea1f65def8742e16
AkaCyberRat <andrejchuk01@gmail.com>
2022-12-28 17:49:40 +0700

    Add show form

---
+++ ProcessMonitor/App/Forms/MoreProcess.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace App.Forms
+{
+    partial class MoreProcessForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxId = new System.Windows.Forms.TextBox();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxMachineName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxPrioClass = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxStartTme = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label1.Location = new System.Drawing.Point(83, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Id:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // textBoxId
+            // 
+            this.textBoxId.Location = new System.Drawing.Point(112, 14);
+            this.textBoxId.Name = "textBoxId";
+            this.textBoxId.ReadOnly = true;
+            this.textBoxId.Size = new System.Drawing.Size(175, 20);
+            this.textBoxId.TabIndex = 1;
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(112, 40);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.ReadOnly = true;
+            this.textBoxName.Size = new System.Drawing.Size(175, 20);
+            this.textBoxName.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label2.Location = new System.Drawing.Point(57, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Name:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // textBoxMachineName
+            // 
+            this.textBoxMachineName.Location = new System.Drawing.Point(112, 66);
+            this.textBoxMachineName.Name = "textBoxMachineName";
+            this.textBoxMachineName.ReadOnly = true;
+            this.textBoxMachineName.Size = new System.Drawing.Size(175, 20);
+            this.textBoxMachineName.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label3.Location = new System.Drawing.Point(4, 66);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "MachineName:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // textBoxPrioClass
+            // 
+            this.textBoxPrioClass.Location = new System.Drawing.Point(112, 92);
+            this.textBoxPrioClass.Name = "textBoxPrioClass";
+            this.textBoxPrioClass.ReadOnly = true;
+            this.textBoxPrioClass.Size = new System.Drawing.Size(175, 20);
+            this.textBoxPrioClass.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label4.Location = new System.Drawing.Point(16, 92);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "PriorityClass:";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // textBoxStartTme
+            // 
+            this.textBoxStartTme.Location = new System.Drawing.Point(112, 118);
+            this.textBoxStartTme.Name = "textBoxStartTme";
+            this.textBoxStartTme.ReadOnly = true;
+            this.textBoxStartTme.Size = new System.Drawing.Size(175, 20);
+            this.textBoxStartTme.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label5.Location = new System.Drawing.Point(33, 118);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(73, 17);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "StartTime:";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 144);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(275, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Ok";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.OnButtonClick);
+            // 
+            // MoreProcess
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(299, 177);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxStartTme);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxPrioClass);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxMachineName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxId);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MoreProcess";
+            this.ShowIcon = false;
+            this.Text = "Details";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxId;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxMachineName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxPrioClass;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxStartTme;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ ProcessMonitor/App/Forms/MoreProcess.cs
@@ -0,0 +1,24 @@
+﻿using App.Core;
+using System.Windows.Forms;
+
+namespace App.Forms
+{
+    internal partial class MoreProcessForm : Form
+    {
+        public MoreProcessForm(Process process)
+        {
+            InitializeComponent();
+
+            textBoxId.Text = process.Id;
+            textBoxName.Text = process.Name;
+            textBoxMachineName.Text = process.MachineName;
+            textBoxPrioClass.Text = process.PriorityClass;
+            textBoxStartTme.Text = process.StartTime.ToString();
+        }
+
+        private void OnButtonClick(object sender, System.EventArgs e)
+        {
+            Close();
+        }
+    }
+}
