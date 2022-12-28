581507059ad7328af225aabdbf8f3db7dbed1d08
AkaCyberRat <andrejchuk01@gmail.com>
2022-12-28 17:49:20 +0700

    Change UI and add functionality

--- ProcessMonitor/App/Forms/MainForm.Designer.cs
+++ ProcessMonitor/App/Forms/MainForm.Designer.cs
@@ -28,35 +28,115 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
             this._dataGridView = new System.Windows.Forms.DataGridView();
+            this._comboBoxInterval = new System.Windows.Forms.ComboBox();
+            this._buttonKill = new System.Windows.Forms.Button();
+            this._buttonMore = new System.Windows.Forms.Button();
+            this.mainFormBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this._buttonPause = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this._dataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mainFormBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // _dataGridView
             // 
             this._dataGridView.AllowUserToAddRows = false;
             this._dataGridView.AllowUserToDeleteRows = false;
+            this._dataGridView.AllowUserToOrderColumns = true;
             this._dataGridView.AllowUserToResizeRows = false;
-            this._dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this._dataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle3.NullValue = null;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this._dataGridView.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle3;
             this._dataGridView.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
-            this._dataGridView.Location = new System.Drawing.Point(12, 12);
+            this._dataGridView.Location = new System.Drawing.Point(12, 39);
             this._dataGridView.MultiSelect = false;
             this._dataGridView.Name = "_dataGridView";
+            this._dataGridView.ReadOnly = true;
             this._dataGridView.RowHeadersVisible = false;
             this._dataGridView.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this._dataGridView.RowsDefaultCellStyle = dataGridViewCellStyle4;
             this._dataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this._dataGridView.Size = new System.Drawing.Size(776, 426);
+            this._dataGridView.Size = new System.Drawing.Size(255, 355);
             this._dataGridView.TabIndex = 0;
+            this._dataGridView.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.OnTableClick);
+            this._dataGridView.Scroll += new System.Windows.Forms.ScrollEventHandler(this.OnTableScroll);
+            // 
+            // _comboBoxInterval
+            // 
+            this._comboBoxInterval.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this._comboBoxInterval.Location = new System.Drawing.Point(100, 11);
+            this._comboBoxInterval.Name = "_comboBoxInterval";
+            this._comboBoxInterval.Size = new System.Drawing.Size(167, 21);
+            this._comboBoxInterval.TabIndex = 1;
+            this._comboBoxInterval.SelectedIndexChanged += new System.EventHandler(this.OnComboBoxIntervalChange);
+            // 
+            // _buttonKill
+            // 
+            this._buttonKill.Enabled = false;
+            this._buttonKill.Location = new System.Drawing.Point(146, 399);
+            this._buttonKill.Name = "_buttonKill";
+            this._buttonKill.Size = new System.Drawing.Size(121, 23);
+            this._buttonKill.TabIndex = 2;
+            this._buttonKill.Text = "Kill";
+            this._buttonKill.UseVisualStyleBackColor = true;
+            this._buttonKill.Click += new System.EventHandler(this.OnKillButtonClick);
+            // 
+            // _buttonMore
+            // 
+            this._buttonMore.Enabled = false;
+            this._buttonMore.Location = new System.Drawing.Point(12, 399);
+            this._buttonMore.Name = "_buttonMore";
+            this._buttonMore.Size = new System.Drawing.Size(121, 23);
+            this._buttonMore.TabIndex = 3;
+            this._buttonMore.Text = "More";
+            this._buttonMore.UseVisualStyleBackColor = true;
+            this._buttonMore.Click += new System.EventHandler(this.OnMoreButtonClick);
+            // 
+            // mainFormBindingSource
+            // 
+            this.mainFormBindingSource.DataSource = typeof(App.Forms.MainForm);
+            // 
+            // _buttonPause
+            // 
+            this._buttonPause.Location = new System.Drawing.Point(12, 10);
+            this._buttonPause.Name = "_buttonPause";
+            this._buttonPause.Size = new System.Drawing.Size(75, 23);
+            this._buttonPause.TabIndex = 4;
+            this._buttonPause.Text = "Pause";
+            this._buttonPause.UseVisualStyleBackColor = true;
+            this._buttonPause.Click += new System.EventHandler(this.OnPauseButtonClick);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(279, 432);
+            this.Controls.Add(this._buttonPause);
+            this.Controls.Add(this._buttonMore);
+            this.Controls.Add(this._buttonKill);
+            this.Controls.Add(this._comboBoxInterval);
             this.Controls.Add(this._dataGridView);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "MainForm";
-            this.Text = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Process monitor";
             ((System.ComponentModel.ISupportInitialize)(this._dataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mainFormBindingSource)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -64,6 +144,11 @@
         #endregion
 
         private System.Windows.Forms.DataGridView _dataGridView;
+        private System.Windows.Forms.ComboBox _comboBoxInterval;
+        private System.Windows.Forms.BindingSource mainFormBindingSource;
+        private System.Windows.Forms.Button _buttonKill;
+        private System.Windows.Forms.Button _buttonMore;
+        private System.Windows.Forms.Button _buttonPause;
     }
 }
 
--- ProcessMonitor/App/Forms/MainForm.cs
+++ ProcessMonitor/App/Forms/MainForm.cs
@@ -1,37 +1,247 @@
-﻿using App.Services.ProcessService;
-using System;
-using System.Threading.Tasks;
+﻿using System;
+using System.Data;
+using System.Linq;
 using System.Windows.Forms;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using App.Services.ProcessService;
+using App.Core;
 
 namespace App.Forms
 {
     internal partial class MainForm : Form
     {
+        const string _pauseText = "Pause";
+        const string _continueText = "Continue";
+
         private readonly IProcessService _processService;
+        private readonly ILogger _logger;
         private readonly Timer _timer;
 
-        public MainForm(IProcessService processService)
-        {
-            InitializeComponent();
 
+        private int? _scrollPosition;
+        private string _selectedRowId;
+        private bool _isPaused;
+        
+
+        public MainForm(ILogger<MainForm> logger, IProcessService processService)
+        {
+            _logger = logger;
             _processService = processService;
-
             _timer = new Timer();
-            _timer.Tick += UpdateTable;
-            _timer.Interval = 500;
+
+            InitializeComponent();
+            InitIntervalComboBox();
+            UpdateTableOnTick(null, null);
+
+            _timer.Tick += UpdateTableOnTick;
             _timer.Enabled = true;
+            _timer.Interval = (int)_comboBoxInterval.SelectedValue;
+
+            _logger.LogInformation("Open main form.");
         }
 
-        private async void UpdateTable(object sender, EventArgs e)
+
+        private void OnPauseButtonClick(object sender, EventArgs e)
         {
-            _timer.Enabled = false;
+            if (!_isPaused)
+            {
+                _isPaused = true;
+                _buttonPause.Text = _continueText;
+            }
+            else
+            {
+                _isPaused = false;
+                _buttonPause.Text = _pauseText;
+            }
+        }
 
-            var processes = await Task.Run(() => { return _processService.GetActiveProcesses(); });
-            _dataGridView.DataSource = processes;
+        private void UpdateTableOnTick(object sender, EventArgs e)
+        {
+            if (_isPaused)
+                return;
 
-            _timer.Enabled = true;
+            UpdateTable();
+        }
+
+        private void OnKillButtonClick(object sender, EventArgs e)
+        {
+            _logger.LogInformation($"Kill selected process. Id: {_selectedRowId}");
+
+            ExceptionHandler(() =>
+            {
+                _processService.KillProcessById(_selectedRowId);
+            });
+            
+            UpdateTable();
+        }
+
+        private void OnMoreButtonClick(object sender, EventArgs e)
+        {
+
+            _logger.LogInformation($"Show more info about process. Id: {_selectedRowId}");
+            ExceptionHandler(() =>
+            {
+                var process = _processService.GetProcessById(_selectedRowId);
+                new MoreProcessForm(process).Show();
+            });
+        }
+
+        private void OnComboBoxIntervalChange(object sender, EventArgs e)
+        {            
+            if (_comboBoxInterval.SelectedValue is int val)
+            {
+                _logger.LogInformation($"Update interval changed to {val} ms.");
+                _timer.Interval = val;
+            }
+        }
+        
+        private void OnTableClick(object sender, DataGridViewCellEventArgs e)
+        {
+            RememberSelectedRow();
+
+            SwitchMoreKillButtonsIfShould();
+        }
+
+        private void OnTableScroll(object sender, ScrollEventArgs e)
+        {
+            RememberScrollPosition();
         }
 
 
+        #region Helpers
+
+        private void UpdateTable()
+        {
+            _logger.LogDebug("Update process list.");
+
+            ExceptionHandler(async () =>
+            {
+                _timer.Enabled = false;
+
+                RememberSelectedRow();
+
+
+                // Get old and current data
+                var oldProcesses = (ProcessCompact[]) _dataGridView.DataSource;
+                var curProcesses = await Task.Run(() => { return _processService.GetActiveProcessesCompact(); });
+                if (oldProcesses == null)
+                {
+                    oldProcesses = new ProcessCompact[] { };
+                }
+
+
+                // Get new and exited processes id's
+                var newProcessesIds = curProcesses.Select(p => p.Id).Except(oldProcesses.Select(p => p.Id));
+                var exitedProcessesIds = oldProcesses.Select(p => p.Id).Except(curProcesses.Select(p => p.Id));
+
+                //Log new processes
+                foreach (var id in newProcessesIds)
+                {
+                    _logger.LogDebug($"New process - Id:{id} Name:{curProcesses.First(p => p.Id == id).Name}");
+                }
+
+                //Log exited processes
+                foreach (var id in exitedProcessesIds)
+                {
+                    _logger.LogDebug($"Stop process - Id:{id} Name:{oldProcesses.First(p => p.Id == id).Name}");
+                }
+
+                // Set new data source
+                _dataGridView.DataSource = curProcesses;
+
+
+                RecoverScrollPosition();
+                RecoverSelectedRow();
+
+                SwitchMoreKillButtonsIfShould();
+
+                _timer.Enabled = true;
+            });
+        }
+        
+        private void ExceptionHandler(Action action)
+        {
+            try
+            {
+                action();
+            }
+            catch(Exception e)
+            {
+                _logger.LogError(e.Message);
+                MessageBox.Show(caption: "Error!", text: e.Message, icon: MessageBoxIcon.Error, buttons: MessageBoxButtons.OK);
+            }
+        }
+
+        private void SwitchMoreKillButtonsIfShould()
+        {
+            if (_dataGridView.SelectedRows.Count > 0)
+            {
+                _buttonKill.Enabled = true;
+                _buttonMore.Enabled = true;
+                return;
+            }
+
+
+            _buttonKill.Enabled = false;
+            _buttonMore.Enabled = false;
+        }
+
+        private void RememberSelectedRow()
+        {
+            var selectedRows = _dataGridView.SelectedRows;
+
+            _selectedRowId = selectedRows.Count > 0 ? selectedRows[0].Cells["Id"].Value as string : null;
+        }
+
+        private void RecoverSelectedRow()
+        {
+            // Try reselect old process
+            if (_selectedRowId != null)
+            {
+                var row = _dataGridView.Rows.Cast<DataGridViewRow>().FirstOrDefault(r => r.Cells["Id"].Value as string == _selectedRowId);
+                if (row != null)
+                {
+                    row.Selected = true;
+                    return;
+                }
+            }
+
+            // Unselect all 
+            foreach (DataGridViewRow row in _dataGridView.SelectedRows)
+                row.Selected = false;
+        }
+
+        private void RememberScrollPosition()
+        {
+            _scrollPosition = _dataGridView.FirstDisplayedCell?.RowIndex;
+        }
+
+        private void RecoverScrollPosition()
+        {
+            if (_scrollPosition != null)
+            {
+                _dataGridView.FirstDisplayedScrollingRowIndex = (int)_scrollPosition;
+            }
+        }
+
+        private void InitIntervalComboBox()
+        {
+            var dataSource = new[] {
+                new{ ValueMember = 500, DisplayMember = "Update interval - 0.5s"},
+                new{ ValueMember =  1000, DisplayMember = "Update interval - 1s"},
+                new{ ValueMember = 5000, DisplayMember = "Update interval - 5s"},
+                new{ ValueMember = 10000, DisplayMember = "Update interval - 10s"},
+            };
+
+            _comboBoxInterval.DataSource = dataSource;
+            _comboBoxInterval.DisplayMember = "DisplayMember";
+            _comboBoxInterval.ValueMember = "ValueMember";
+
+            _comboBoxInterval.SelectedValue = 1000;
+        }
+
+        #endregion
     }
 }
+
